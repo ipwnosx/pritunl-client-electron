d4115d420d2367a740818f1f3b39914793c913c2
Zachary Huff <zach.huff.386@gmail.com>
2026-07-10 10:16:17 -0400

    Add zero to profile import component

--- client/app/components/ProfileImport.tsx
+++ client/app/components/ProfileImport.tsx
@@ -1,15 +1,12 @@
 /// <reference path="../References.d.ts"/>
 import * as React from "react"
-import * as Theme from "../Theme"
-import ProfilesStore from "../stores/ProfilesStore"
-import * as ProfileTypes from "../types/ProfileTypes"
-import * as ProfileActions from "../actions/ProfileActions"
-import * as ServiceActions from "../actions/ServiceActions"
+import * as ZeroActions from "../actions/ZeroActions"
+import * as ZeroUtils from "../utils/ZeroUtils"
+import * as Alert from "../Alert"
 import * as Blueprint from "@blueprintjs/core"
-import PageInfo from "./PageInfo"
+import Help from "./Help"
 import PageInput from "./PageInput"
 import PageInputFile from "./PageInputFile"
-import PageSwitch from "./PageSwitch"
 import * as Importer from "../utils/Importer"
 import path from "path"
 
@@ -21,9 +18,17 @@ interface State {
 	disabled: boolean
 	changed: boolean
 	dialog: boolean
+	mode: string
 	uri: string
 	path: string
 	fullPath: string
+	zeroServer: string
+	zeroCard: ZeroUtils.SmartCard
+	zeroKeys: string[]
+	zeroKeysLoaded: boolean
+	zeroSelectedKey: string
+	zeroPath: string
+	zeroFullPath: string
 }
 
 const css = {
@@ -37,13 +42,24 @@ const css = {
 		width: "340px",
 		position: "absolute",
 	} as React.CSSProperties,
+	group: {
+		margin: "0 0 15px 0",
+	} as React.CSSProperties,
 	label: {
 		width: "100%",
-		maxWidth: "220px",
-		margin: "18px 0 0 0",
+		maxWidth: "280px",
 	} as React.CSSProperties,
 	input: {
 		width: "100%",
+	} as React.CSSProperties,
+	select: {
+		width: "100%",
+	} as React.CSSProperties,
+	selectBox: {
+		display: "block",
+		width: "100%",
+		maxWidth: "280px",
+		marginTop: "1px",
 	} as React.CSSProperties,
 }
 
@@ -54,13 +70,55 @@ export default class ProfileImport extends React.Component<Props, State> {
 			disabled: false,
 			changed: false,
 			dialog: false,
+			mode: "vpn",
 			uri: "",
 			path: "",
 			fullPath: "",
+			zeroServer: "",
+			zeroCard: null,
+			zeroKeys: [],
+			zeroKeysLoaded: false,
+			zeroSelectedKey: "",
+			zeroPath: "",
+			zeroFullPath: "",
 		}
 	}
 
-	onImport = (): void => {
+	loadZeroKeys = (): void => {
+		ZeroActions.listKeys().then(
+			(keyOptions: ZeroActions.KeyOptions): void => {
+
+				let selectedKey = this.state.zeroSelectedKey
+				if (!selectedKey && !this.state.zeroFullPath) {
+					if (keyOptions.card) {
+						selectedKey = "card"
+					} else if (keyOptions.keys.length) {
+						selectedKey = keyOptions.keys[0]
+					}
+				}
+
+				this.setState({
+					...this.state,
+					zeroCard: keyOptions.card,
+					zeroKeys: keyOptions.keys,
+					zeroKeysLoaded: true,
+					zeroSelectedKey: selectedKey,
+				})
+			})
+	}
+
+	setMode = (mode: string): void => {
+		this.setState({
+			...this.state,
+			mode: mode,
+		})
+
+		if (mode === "zero" && !this.state.zeroKeysLoaded) {
+			this.loadZeroKeys()
+		}
+	}
+
+	onImportVpn = (): void => {
 		this.setState({
 			...this.state,
 			disabled: true,
@@ -93,11 +151,82 @@ export default class ProfileImport extends React.Component<Props, State> {
 		}
 	}
 
+	onImportZero = (): void => {
+		let cardSerial = ""
+		let publicKeyPath = ""
+
+		if (this.state.zeroFullPath !== "") {
+			publicKeyPath = this.state.zeroFullPath
+		} else if (this.state.zeroSelectedKey === "card") {
+			if (this.state.zeroCard) {
+				cardSerial = this.state.zeroCard.serial
+			}
+		} else if (this.state.zeroSelectedKey) {
+			publicKeyPath = ZeroUtils.SSH_DIR + "/" +
+				this.state.zeroSelectedKey
+		}
+
+		if (!cardSerial && !publicKeyPath) {
+			Alert.error("No SSH key selected, run \"ssh-keygen -t ed25519\" " +
+				"to create a key")
+			return
+		}
+
+		this.setState({
+			...this.state,
+			disabled: true,
+		})
+
+		ZeroActions.importZero({
+			server: this.state.zeroServer,
+			publicKeyPath: publicKeyPath,
+			cardSerial: cardSerial,
+		}).then((success: boolean): void => {
+			if (!success) {
+				this.setState({
+					...this.state,
+					disabled: false,
+				})
+				return
+			}
+
+			if (cardSerial) {
+				Alert.info("Smart Card must be registered with Pritunl " +
+					"Zero, use Register Card on the profile to register " +
+					"the device", 10)
+			}
+
+			this.setState({
+				...this.state,
+				dialog: false,
+				disabled: false,
+				changed: false,
+				zeroServer: "",
+				zeroKeysLoaded: false,
+				zeroSelectedKey: "",
+				zeroPath: "",
+				zeroFullPath: "",
+			})
+		})
+	}
+
+	onImport = (): void => {
+		if (this.state.mode === "zero") {
+			this.onImportZero()
+		} else {
+			this.onImportVpn()
+		}
+	}
+
 	openDialog = (): void => {
 		this.setState({
 			...this.state,
 			dialog: true,
 		})
+
+		if (this.state.mode === "zero" && !this.state.zeroKeysLoaded) {
+			this.loadZeroKeys()
+		}
 	}
 
 	closeDialog = (): void => {
@@ -108,6 +237,33 @@ export default class ProfileImport extends React.Component<Props, State> {
 	}
 
 	render(): JSX.Element {
+		let zeroMode = this.state.mode === "zero"
+
+		let importDisabled = this.state.disabled
+		if (zeroMode) {
+			importDisabled = importDisabled || !this.state.zeroServer
+		} else {
+			importDisabled = importDisabled || !this.state.changed
+		}
+
+		let zeroKeyOptions: JSX.Element[] = []
+
+		if (this.state.zeroCard) {
+			zeroKeyOptions.push(<option key="card" value="card">
+				{"Smart Card (" + this.state.zeroCard.serial + ")"}
+			</option>)
+		}
+
+		for (let key of this.state.zeroKeys) {
+			zeroKeyOptions.push(<option key={key} value={key}>{key}</option>)
+		}
+
+		if (!zeroKeyOptions.length) {
+			zeroKeyOptions.push(<option key="" value="">
+				No SSH keys found
+			</option>)
+		}
+
 		return <div style={css.box}>
 			<button
 				className="bp5-button bp5-minimal bp5-icon-import"
@@ -127,39 +283,125 @@ export default class ProfileImport extends React.Component<Props, State> {
 				onClose={this.closeDialog}
 			>
 				<div className="bp5-dialog-body">
-					<PageInput
-						disabled={this.state.disabled}
-						label="Profile URI"
-						help="Profile URI as shown in the Pritunl server web console."
-						type="text"
-						placeholder="Enter URI"
-						value={this.state.uri}
-						onChange={(val: string): void => {
-							this.setState({
-								...this.state,
-								changed: true,
-								uri: val,
-								path: "",
-								fullPath: "",
-							})
-						}}
-					/>
-					<PageInputFile
-						disabled={this.state.disabled}
-						label="Import Profile"
-						help="Select profile file in tar, zip, ovpn or conf format."
-						accept=".ovpn,.conf,.tar,.zip"
-						value={this.state.path}
-						onChange={(val: string): void => {
-							this.setState({
-								...this.state,
-								changed: true,
-								uri: "",
-								path: path.basename(val),
-								fullPath: val,
-							})
-						}}
-					/>
+					<div className="bp5-button-group bp5-fill" style={css.group}>
+						<button
+							className={"bp5-button bp5-icon-globe-network" +
+								(!zeroMode ? " bp5-active" : "")}
+							type="button"
+							disabled={this.state.disabled}
+							onClick={(): void => {
+								this.setMode("vpn")
+							}}
+						>Pritunl VPN</button>
+						<button
+							className={"bp5-button bp5-icon-key" +
+								(zeroMode ? " bp5-active" : "")}
+							type="button"
+							disabled={this.state.disabled}
+							onClick={(): void => {
+								this.setMode("zero")
+							}}
+						>Pritunl Zero</button>
+					</div>
+					<div hidden={zeroMode}>
+						<PageInput
+							disabled={this.state.disabled}
+							label="Profile URI"
+							help="Profile URI as shown in the Pritunl server web console."
+							type="text"
+							placeholder="Enter URI"
+							value={this.state.uri}
+							onChange={(val: string): void => {
+								this.setState({
+									...this.state,
+									changed: true,
+									uri: val,
+									path: "",
+									fullPath: "",
+								})
+							}}
+						/>
+						<PageInputFile
+							disabled={this.state.disabled}
+							label="Import Profile"
+							help="Select profile file in tar, zip, ovpn or conf format."
+							accept=".ovpn,.conf,.tar,.zip"
+							value={this.state.path}
+							onChange={(val: string): void => {
+								this.setState({
+									...this.state,
+									changed: true,
+									uri: "",
+									path: path.basename(val),
+									fullPath: val,
+								})
+							}}
+						/>
+					</div>
+					<div hidden={!zeroMode}>
+						<PageInput
+							disabled={this.state.disabled}
+							label="Pritunl Zero Server"
+							help="Pritunl Zero user hostname as shown in the Pritunl Zero web console."
+							type="text"
+							placeholder="Enter server hostname"
+							value={this.state.zeroServer}
+							onChange={(val: string): void => {
+								this.setState({
+									...this.state,
+									changed: true,
+									zeroServer: val,
+								})
+							}}
+						/>
+						<label
+							className="bp5-label"
+							style={css.label}
+						>
+							SSH Key
+							<Help
+								title="SSH Key"
+								content="Select the SSH key or Smart Card device that will be signed by the Pritunl Zero server."
+							/>
+							<div
+								className="bp5-html-select"
+								style={css.selectBox}
+							>
+								<select
+									style={css.select}
+									disabled={this.state.disabled}
+									value={this.state.zeroSelectedKey}
+									onChange={(evt): void => {
+										this.setState({
+											...this.state,
+											changed: true,
+											zeroSelectedKey: evt.target.value,
+											zeroPath: "",
+											zeroFullPath: "",
+										})
+									}}
+								>
+									{zeroKeyOptions}
+								</select>
+							</div>
+						</label>
+						<PageInputFile
+							disabled={this.state.disabled}
+							label="SSH Public Key File"
+							help="Select SSH public key file ending in .pub to use a key that is not in the SSH directory."
+							accept=".pub"
+							value={this.state.zeroPath}
+							onChange={(val: string): void => {
+								this.setState({
+									...this.state,
+									changed: true,
+									zeroSelectedKey: "",
+									zeroPath: path.basename(val),
+									zeroFullPath: val,
+								})
+							}}
+						/>
+					</div>
 				</div>
 				<div className="bp5-dialog-footer">
 					<div className="bp5-dialog-footer-actions">
@@ -172,7 +414,7 @@ export default class ProfileImport extends React.Component<Props, State> {
 						<button
 							className="bp5-button bp5-intent-success bp5-icon-tick"
 							type="button"
-							disabled={this.state.disabled || !this.state.changed}
+							disabled={importDisabled}
 							onClick={this.onImport}
 						>Import</button>
 					</div>
